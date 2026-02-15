75ed2e23275a753dd165323673733e588771bb68
Kevin Stock <kevin@kevinstock.org>
2026-02-15 08:51:13 -0500

    Add tonic HelloWorld RPC

--- crates/common/build.rs
+++ crates/common/build.rs
@@ -1,7 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/hello.proto");
 
-    tonic_build::configure().compile_protos(&["proto/hello.proto"], &["proto"])?;
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+    let mut prost_config = prost_build::Config::new();
+    prost_config.protoc_executable(protoc);
+
+    tonic_build::configure().compile_protos_with_config(
+        prost_config,
+        &["proto/hello.proto"],
+        &["proto"],
+    )?;
 
     Ok(())
 }
